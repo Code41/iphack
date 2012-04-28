305c2fa11460895206c42941f83c9d79c40966ab
CJ <cjbbrooking@gmail.com>
2012-04-28 19:06:52 +0100

    frigged it to work by putting the call into the toplist.run() event. The sequence now waits for TopList to generate before starting

--- powerhour/samplr/samplr.js
+++ powerhour/samplr/samplr.js
@@ -5,6 +5,7 @@ var player = models.player;
 exports.init = init;
 var allTracks = new Array();
 
+
 function init() {
 
 var toplist = new models.Toplist();
@@ -22,21 +23,25 @@ toplist.observe(models.EVENT.CHANGE, function() {
 	var i;
 	currentTrack = toplist.results[0].name;
 	currentTrackUri = toplist.results[0].uri;
-	console.log(currentTrackUri);	
+	console.log("Debug: currentTrackUri:" + currentTrackUri);
+	
     
 	for (i=0;i<60;i++)  // go to 60 tracks
 	{	
 		add_li("bands", i + 1 + ". " + toplist.results[i]);	
 		allTracks[i] = toplist.results[i];	
 	}		
+	doNextPlay(n,n);	//play next in T sec
 	playTrack(toplist.results[i]);	
 });
 
 	toplist.run();
-	console.log(toplist);
+	
+	
+	//console.log(toplist);
 
 	var n = 60;
-	doNextPlay(n,n);	//play next in T sec
+	//doNextPlay(n,n);	//play next in T sec
 }
 
 function update_nowplaying(song) {
@@ -65,31 +70,23 @@ function doNextPlay(n, max)
 {
 	console.log("Debug: doNextPlay(" + n + "," + max + ")");
 	n--;
-	console.log("N decremented:" + n);
+	console.log("Debug: N decremented:" + n);
 	
 	if (n > 0) //if n==0, then skip and exit
 	{		
 		playTrack(getTopTrackNUri(n));	//start playing track		
-		var t=setTimeout(function() { doNextPlay(n,max) },5000); // get and play next track in 60s			
+		var t=setTimeout(function() { doNextPlay(n,max) },2000); // get and play next track in 60s			
 	}
 }
 
 function getTopTrackNUri(n) // get the n'th top track
 {
 	console.log("Debug: GetTopTrack(" + n + ")");
+	var track = new models.Track();
+	track = allTracks[n].uri;
+
+	console.log("Debug: Track[" + n + "]=" + track);
 	
-	console.log(allTracks);
-	console.log(allTracks[0]);
-	console.log(allTracks[0].uri);
-	
-	console.log("ST1:" + allTracks.results[59].uri);
-	
-	startTrack=toplist.results[n].uri;	
-	
-	console.log("ST2:" + startTrack);
-	
-	
-	//return n;
-	return startTrack;
+	return track;
 }
 
