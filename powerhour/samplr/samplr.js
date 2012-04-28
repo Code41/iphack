4631cae4178c93319a8e145e021c347572c0411d
Paul Reid <paulyreid@gmail.com>
2012-04-28 16:51:39 +0100

    css styling

--- powerhour/samplr/samplr.js
+++ powerhour/samplr/samplr.js
@@ -14,21 +14,36 @@ toplist.toplistType = models.TOPLISTTYPE.REGION;
 toplist.matchType = models.TOPLISTMATCHES.TRACKS;  //Choose tracks
 toplist.region = "GB";  // use GB region
 
-var startTrack 
+var currentcountdown;
+currentcountdown = 60;
+var countnum = document.getElementById('countnum');
+var nowplaying = document.getElementById('nowplaying');
+var currentTrackUri;
+var currentTrack;
 
 // update playlist with top 60 tracks
 toplist.observe(models.EVENT.CHANGE, function() {    	
 	var i;
-	startTrack = toplist.results[0].uri;
-	console.log(startTrack);
-	for (i=0;i<6;i++)  // go to 60 tracks
+	currentTrack = toplist.results[0].name;
+	currentTrackUri = toplist.results[0].uri;
+	console.log(currentTrackUri);	
+    
+	for (i=0;i<10;i++)  // go to 60 tracks
 	{	
-		add_li("bands", i + 1 + " :" + toplist.results[i]);		
-	}
-	playTrack(startTrack);
+		add_li("bands", i + 1 + ". " + toplist.results[i]);	
+		console.log(currentcountdown);
+		currentcountdown = currentcountdown - 1;
+		countnum.innerHTML = currentcountdown;	
+		update_nowplaying(toplist.results[i])
+	}		
+	playTrack(toplist.results[i]);	
 });
 
 toplist.run(); //execute the toplist
+}
+
+function update_nowplaying(song) {
+nowplaying.innerHTML = song;
 }
 
 function add_li(list, text) {
