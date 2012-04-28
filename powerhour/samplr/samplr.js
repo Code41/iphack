129981eba128d218eeb4dadd20b87682e534f25d
CJ <cjbbrooking@gmail.com>
2012-04-28 13:59:44 +0100

    Added comments to samplr.js

--- powerhour/samplr/samplr.js
+++ powerhour/samplr/samplr.js
@@ -8,23 +8,30 @@ function init() {
 var sp = getSpotifyApi(1);
 var models = sp.require('sp://import/scripts/api/models');
 
+// Create a toplist of the to 60 tracks
 var toplist = new models.Toplist();
 toplist.toplistType = models.TOPLISTTYPE.REGION;
-toplist.matchType = models.TOPLISTMATCHES.TRACKS;
-toplist.region = "GB";
+toplist.matchType = models.TOPLISTMATCHES.TRACKS;  //Choose tracks
+toplist.region = "GB";  // use GB region
 
+// Create a playlist to put the top 60 into
 var myAwesomePlaylist = new models.Playlist("My Awesome Playlist");
 
+// update playlist with top 60 tracks
 toplist.observe(models.EVENT.CHANGE, function() {    	
 	var i;
-	for (i=0;i<60;i++)
+	for (i=0;i<60;i++)  // go to 60 tracks
 	{	
 		add_li("bands", i + 1 + " :" + toplist.results[i]);		
-		myAwesomePlaylist.add(toplist.results[i]);
+		myAwesomePlaylist.add(toplist.results[i]);  // add track to playlist
 	}
 });
 
-toplist.run();
+toplist.run(); //execute the toplist
+
+
+
+
 }
 
 function add_li(list, text) {
