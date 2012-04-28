6c166e650190749e682c7f3d4de5d24e01e42572
Paul Reid <paulyreid@gmail.com>
2012-04-28 13:47:02 +0100

    first power hour app

---
+++ powerhour/samplr/samplr.js
@@ -0,0 +1,35 @@
+var sp = getSpotifyApi(1);
+var models = sp.require('sp://import/scripts/api/models');
+var player = models.player;
+
+exports.init = init;
+
+function init() {
+var sp = getSpotifyApi(1);
+var models = sp.require('sp://import/scripts/api/models');
+
+var toplist = new models.Toplist();
+toplist.toplistType = models.TOPLISTTYPE.REGION;
+toplist.matchType = models.TOPLISTMATCHES.TRACKS;
+toplist.region = "GB";
+
+var myAwesomePlaylist = new models.Playlist("My Awesome Playlist");
+
+toplist.observe(models.EVENT.CHANGE, function() {    	
+	var i;
+	for (i=0;i<60;i++)
+	{	
+		add_li("bands", i + 1 + " :" + toplist.results[i]);		
+		myAwesomePlaylist.add(toplist.results[i]);
+	}
+});
+
+toplist.run();
+}
+
+function add_li(list, text) {
+var list = document.getElementById(list);
+var li = document.createElement("li");
+li.innerHTML = text;
+list.appendChild(li);
+}
